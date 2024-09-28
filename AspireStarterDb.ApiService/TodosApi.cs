93f260a9945d26f7e0132275ff142ae21c824796
Damian Edwards <damian@damianedwards.com>
2024-09-27 17:59:50 -0700

    Add CRUD to the TodosApi

--- AspireStarterDb.ApiService/TodosApi.cs
+++ AspireStarterDb.ApiService/TodosApi.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using AspireStarterDb.ApiDbModel;
+using AspireStarterDb.ApiService;
 
 namespace Microsoft.Extensions.Hosting;
 
@@ -9,9 +10,51 @@ public static class TodosApi
     {
         var todos = app.MapGroup("/todos");
 
-        todos.MapGet("/", (TodosDbContext db) => db.Todos);
+        todos.MapGet("/", (TodosDbContext db) => db.Todos.ToListAsync());
 
         todos.MapGet("/{id:int}", (int id, TodosDbContext db) => db.Todos.FirstOrDefaultAsync(t => t.Id == id));
+
+        todos.MapPost("/", async (Todo todo, TodosDbContext db) =>
+        {
+            if (!ValidationHelper.IsValid(todo, out var validationErrors))
+            {
+                return Results.ValidationProblem(validationErrors);
+            }
+
+            db.Todos.Add(todo);
+            await db.SaveChangesAsync();
+            return Results.Created($"/api/Todo/{todo.Id}", todo);
+        });
+
+        todos.MapPut("/{id}", async (int id, Todo todo, TodosDbContext db) =>
+        {
+            if (id != todo.Id)
+            {
+                return Results.BadRequest();
+            }
+
+            if (!ValidationHelper.IsValid(todo, out var validationErrors))
+            {
+                return Results.ValidationProblem(validationErrors);
+            }
+
+            var affected = await db.Todos
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                    .SetProperty(t => t.Title, todo.Title)
+                    .SetProperty(t => t.IsComplete, todo.IsComplete)
+                );
+
+            return affected == 1 ? Results.Ok() : Results.NotFound();
+        });
+
+        todos.MapDelete("/{id}", async (int id, TodosDbContext db) =>
+        {
+            var affected = await db.Todos
+                .Where(todo => todo.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? Results.Ok() : Results.NotFound();
+        });
 
         return app;
     }
