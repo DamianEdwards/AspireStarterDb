a8530603f9df5b906a9161b07e1b6dc4383df21b
Damian Edwards <damian@damianedwards.com>
2024-10-04 18:01:45 -0700

    Updates from ASP.NET Core 9 templates

--- AspireStarterDb.ApiService/Program.cs
+++ AspireStarterDb.ApiService/Program.cs
@@ -10,10 +10,18 @@ builder.AddSqlServerDbContext<TodosDbContext>("todosdb");
 // Add services to the container.
 builder.Services.AddProblemDetails();
 
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.MapWeatherApi();
 
